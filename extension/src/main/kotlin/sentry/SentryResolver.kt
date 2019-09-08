183e670536542263d3bb6ec5cf56b0d703903a71
Jan Galinski <jan.galinski@holisticon.de>
2019-09-08 16:11:41 +0200

    refactore when -> if/else for readability

--- extension/src/main/kotlin/sentry/SentryResolver.kt
+++ extension/src/main/kotlin/sentry/SentryResolver.kt
@@ -4,9 +4,13 @@ import io.holunda.extension.casemanagement.bpmn.CaseTaskDefinition
 import io.holunda.extension.casemanagement.expressionManager
 import org.camunda.bpm.engine.delegate.DelegateExecution
 
-fun evaluateSentryCondition(execution: DelegateExecution, caseTaskDefinition: CaseTaskDefinition): Boolean = when (caseTaskDefinition.hasSentry) {
-  true -> execution.expressionManager
-    .createExpression(caseTaskDefinition.sentryOnPartExpression)
-    .getValue(execution, null) as Boolean
-  false -> true
-}
+/**
+ * Executes the expression configured on caseTaskDefinition (if present).
+ */
+fun evaluateSentryCondition(execution: DelegateExecution, caseTaskDefinition: CaseTaskDefinition): Boolean =
+  if (caseTaskDefinition.hasSentry)
+    execution.expressionManager
+      .createExpression(caseTaskDefinition.sentryOnPartExpression)
+      .getValue(execution, null) as Boolean
+  else true
+
